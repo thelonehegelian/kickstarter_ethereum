1f15d0b448b5286d707972debc0d41405f6e9e3b
thelonehegelian <thelonehegelian@gmail.com>
2022-03-16 22:41:37 +0000

    fix contract deployment issue
    interface and bytecode are received as JavaScript object from the compile filed so JSON.parse() is not required during contract creation

--- 2_lottery/compile.js
+++ 2_lottery/compile.js
@@ -2,15 +2,15 @@ const path = require("path");
 const solc = require("solc");
 const fs = require("fs");
 
+// get ath of the contract
+let contractPath = path.resolve(__dirname, "contracts", "Lottery.sol");
+
 // compilation using the latest version of solc
 let input = {
   language: "Solidity",
   sources: {
     "Lottery.sol": {
-      content: fs.readFileSync(
-        path.resolve(__dirname, "contracts", "Lottery.sol"),
-        "utf8"
-      ),
+      content: fs.readFileSync(contractPath, "utf8"),
     },
   },
   settings: {
@@ -30,11 +30,13 @@ let output = JSON.parse(solc.compile(JSON.stringify(input)));
 // export abi and bytecode compiled contract
 
 const interface = output.contracts["Lottery.sol"].Lottery.abi;
-const bytecode = output.contracts["Lottery.sol"].Lottery.evm.bytecode;
+const bytecode = output.contracts["Lottery.sol"].Lottery.evm.bytecode.object;
 
 module.exports = {
   interface,
   bytecode,
 };
+
+console.log(`Contract compiled successfully`);
 
 // console.log(output.contracts['Lottery.sol'].Lottery.evm.bytecode.object)
--- 2_lottery/test/Lottery.test.js
+++ 2_lottery/test/Lottery.test.js
@@ -5,7 +5,32 @@ const Web3 = require("web3");
 // instantiate web3 with a ganache provider
 const web3 = new Web3(ganache.provider());
 // import the interface and bytecode from the contract object
-// the compiled code is in JSON format
-// JSON.parse would need to be used on it to convert it to a JavaScript to interact with it using the web3 library
-// bytecode = evm.bytecode.object
+// the compiled code is in JS Object format
+// JSON.parse wont be needed as parsed it during the compilation process in compile.js
 const { interface, bytecode } = require("../compile");
+
+// console.log(bytecode);
+// variables
+
+let accounts;
+let lotteryContract;
+let txReceipt;
+
+beforeEach(async () => {
+  // get acounts
+  accounts = await web3.eth.getAccounts();
+  console.log(accounts);
+  // deploy the contract
+  lotteryContract = await new web3.eth.Contract(interface)
+    .deploy({
+      data: bytecode,
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+// test to validate contract deployment
+describe("Lottery", () => {
+  it("deploys a contract", () => {
+    // check to see if the contract has an address, if it does then it was deployed successfully
+    console.log(lotteryContract.options.address);
+  });
+});
