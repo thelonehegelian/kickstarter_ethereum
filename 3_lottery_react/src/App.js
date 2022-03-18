c7e6d54c107527a3a35b82f8940c848455c4c318
thelonehegelian <thelonehegelian@gmail.com>
2022-03-18 21:17:33 +0000

    Get lottery managers address and set it as state

--- 3_lottery_react/src/App.js
+++ 3_lottery_react/src/App.js
@@ -5,10 +5,23 @@ import web3 from "./web3";
 import lottery from "./lotteryContract";
 
 class App extends React.Component {
+  // Define initial state
+  constructor(props) {
+    super(props);
+
+    this.state = { manager: "" };
+  }
+
+  // TODO: use setState and useEffect() and  for this call
+  async componentDidMount() {
+    // function calls are invoked from a default metamask address set during the initial setup
+    // this means that the .call({from: ''}) does not need to have a 'from' field
+    const manager = await methods.manager.call();
+    // update state
+    this.setState({ manager: manager });
+  }
   render() {
     console.log(web3.version); // version: 1.7.1
-    console.log(lottery);
-
     return (
       <div className="App">
         <header className="App-header">
