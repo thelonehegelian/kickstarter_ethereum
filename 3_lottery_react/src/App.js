89e285b3bb4cefad88ecd95fb1ea22f7a59d3e7d
thelonehegelian <thelonehegelian@gmail.com>
2022-03-19 21:27:04 +0000

    Enable use to enter lottery through onSubmit handler

--- 3_lottery_react/src/App.js
+++ 3_lottery_react/src/App.js
@@ -10,6 +10,7 @@ class App extends React.Component {
     manager: "",
     players: [],
     balance: "",
+    value: "",
   };
 
   // TODO: use setState and useEffect() and  for this call
@@ -26,6 +27,23 @@ class App extends React.Component {
     // update state
     this.setState({ manager: manager, players: players.length });
   }
+  // form submit handler
+  // with arrow function syntax: this = component
+  onSubmit = async (event) => {
+    // prevent form from rerendering the component
+    event.preventDefault();
+    // this gets the connected accounts from the users Metamask
+    const accounts = await web3.eth.getAccounts();
+
+    // enter lottery by sending a transaction with specific amount of ETH
+    await lottery.methods.enter().send({
+      from: accounts[0],
+      // get 'value' from the state as it is derived from the form input
+      // the user enter the amount in ETH, so needs to be converted to Wei
+      value: web3.utils.toWei(this.state.value, "ether"),
+    });
+    console.log(accounts[0]);
+  };
   render() {
     console.log(`Web3 version: ${web3.version}`); // version: 1.7.1
     // console logs the connected metamsk account
@@ -38,13 +56,18 @@ class App extends React.Component {
         <h1>Lottery</h1>
         <p>Hello, I am ze Manakher</p>
         <br />
-        <p>My address is: </p>
+        <p>My address is:</p>
         <br />
         <p>
-          There are currently {this.state.players} players in the lottery,
-          {/*Coverts wei to ether*/}
+          There are currently {this.state.players}
+          players in the lottery, {/* Coverts wei to ether */}
           playing to win {web3.utils.fromWei(this.state.balance, "ether")}
         </p>
+        <form onSubmit={this.onSubmit}>
+          <label>Amount of Ether to enter</label>
+          <input />
+          <button>Enter</button>
+        </form>
       </div>
     );
   }
