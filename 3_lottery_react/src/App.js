28cd6bc9278b1c7b43202cc0dd9d2c99ca7a8ed5
thelonehegelian <thelonehegelian@gmail.com>
2022-03-19 17:17:58 +0000

    Add comments to App.js

--- 3_lottery_react/src/App.js
+++ 3_lottery_react/src/App.js
@@ -16,7 +16,9 @@ class App extends React.Component {
   async componentDidMount() {
     // function calls are invoked from a default metamask address set during the initial setup
     // this means that the .call({from: ''}) does not need to have a 'from' field
-    const manager = await lottery.methods;
+    const manager = await lottery.methods.manager().call();
+    //BUG: currently this is rturning an empty array for some reason
+    console.log(manager);
     const players = await lottery.methods.getPlayers().call();
     // this will be an object wrapped in BigNumber.js library
     const balance = await web3.eth.getBalance(lottery.options.address);
@@ -26,16 +28,11 @@ class App extends React.Component {
   }
   render() {
     console.log(`Web3 version: ${web3.version}`); // version: 1.7.1
+    // console logs the connected metamsk account
     // web3.eth.getAccounts().then((accounts) => {
     //   console.log("Connected", accounts);
     // });
 
-    lottery.methods
-      .manager()
-      .call()
-      .then((managerAccount) => {
-        console.log(managerAccount);
-      });
     return (
       <div className="App">
         <h1>Lottery</h1>
