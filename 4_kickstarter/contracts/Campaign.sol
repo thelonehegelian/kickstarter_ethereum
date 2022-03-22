2303c70a3a664b9882079d995afce330ec3b9dbc
thelonehegelian <thelonehegelian@gmail.com>
2022-03-22 02:03:15 +0000

    Fix constructor function

--- 4_kickstarter/contracts/Campaign.sol
+++ 4_kickstarter/contracts/Campaign.sol
@@ -13,44 +13,36 @@ requests => Request []
 */
 
 // Campaign => constructor
-contract Campaign  {
+contract Campaign {
+    address payable public manager;
     // minimum Contribution in Wei
-    uint public minimumContribution;
+    uint256 public minimumContribution;
+
     // constructor function requires a minimum amount of ETH to create contract
     // the Contract cannot be deployed without this
-    // as soon as you add the constructor function with a required parameter 
+    // as soon as you add the constructor function with a required parameter
     // remix deploy button adds an Iput field
-    constructor  (uint _minimum)  {
-            minimumContribution = _minimum;
+    constructor(uint256 _minimum) {
+        minimumContribution = _minimum;
+        manager = payable(msg.sender);
     }
 
-    // set creator of the contract as it's manager 
-    address public  manager = msg.sender;
-    
     // approvers array updated at approveRequest function call
-    address payable [] public approvers;
-    address [] public contributors;
- 
+    address payable[] public approvers;
+    address[] public contributors;
+
     //function to allow users to participate in the Campaign
     // send minimum amount of eth required to participate in the Campaign
-    function contribute  () public payable {
-        
+    function contribute() public payable {
         // ADD REQUIRE STATEMENT MODIFIER HERE
-        
-        // push the function invocators address to the contributors array  
+        // push the function invocators address to the contributors array
+    }
 
-    } 
     // Called by the manager to create a 'spending request' [how to determine a valid spending request in the real world?]
-    function createRequest () public {
+    function createRequest() public {}
 
-    }
-    // Called by the contributors to approve a spending request 
-    function approveRequest() public {
+    // Called by the contributors to approve a spending request
+    function approveRequest() public {}
 
-    }
-    function finalizeRequest () public {
-
-    }
-
+    function finalizeRequest() public {}
 }
-    
