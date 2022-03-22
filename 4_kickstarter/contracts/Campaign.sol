61a579d41410b4eb1179ba50acb3fa38e83feaa8
thelonehegelian <thelonehegelian@gmail.com>
2022-03-22 02:12:39 +0000

    Implement contribute function

--- 4_kickstarter/contracts/Campaign.sol
+++ 4_kickstarter/contracts/Campaign.sol
@@ -9,12 +9,15 @@ manager => address [who is the manager in the real world application?]
 minimumContribution => uint
 approvers => address array
 requests => Request []
-
 */
 
 // Campaign => constructor
 contract Campaign {
+    // manager is also the creator of the contract
     address payable public manager;
+    // approvers array updated at approveRequest function call
+    address payable[] public approvers;
+    address[] public contributors;
     // minimum Contribution in Wei
     uint256 public minimumContribution;
 
@@ -24,18 +27,18 @@ contract Campaign {
     // remix deploy button adds an Iput field
     constructor(uint256 _minimum) {
         minimumContribution = _minimum;
+        // set manager as the creator of the contract
         manager = payable(msg.sender);
     }
-
-    // approvers array updated at approveRequest function call
-    address payable[] public approvers;
-    address[] public contributors;
 
     //function to allow users to participate in the Campaign
     // send minimum amount of eth required to participate in the Campaign
     function contribute() public payable {
+        // the amount sent at invocation should be larger that minimumContribution
         // ADD REQUIRE STATEMENT MODIFIER HERE
+        require(msg.value > minimumContribution);
         // push the function invocators address to the contributors array
+        approvers.push(payable(msg.sender));
     }
 
     // Called by the manager to create a 'spending request' [how to determine a valid spending request in the real world?]
