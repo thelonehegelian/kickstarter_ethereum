62fa7cb44e960cbd0bef355baea99662959ea7c1
thelonehegelian <thelonehegelian@gmail.com>
2022-04-03 00:46:41 +0100

    Update compile file to output more than one contract as json file in the build folder

--- 4_kickstarter/ethereum/compile.js
+++ 4_kickstarter/ethereum/compile.js
@@ -1,15 +1,33 @@
 const path = require("path");
 const solc = require("solc");
-const fs = require("fs");
+const fs = require("fs-extra");
+
+
+/* 
+1. delete build folder if there is any
+2. create build folder
+3. compile contracts to build folder
+
+*/ 
+
+// entry contract name 
+let entryContractName = 'Campaign.sol' 
+
+// create 'build' folder path
+const buildFolderPath = path.resolve(__dirname, 'build')
+
+// remove build folder and everything inside it
+fs.removeSync(buildFolderPath)
 
 // get path of the contract
-let contractPath = path.resolve(__dirname, "contracts", "Campaign.sol");
+let contractPath = path.resolve(__dirname, "contracts", entryContractName); // contractFolder, nameOfTheFile
+
 
 // compilation using the latest version of solc
 let input = {
   language: "Solidity",
   sources: {
-    "Campaign.sol": {
+    'Campaign.sol': {
       content: fs.readFileSync(contractPath, "utf8"),
     },
   },
@@ -25,17 +43,27 @@ let input = {
 
 console.log(`Compiling contract...`);
 
+// before contract compilation create 'build' path
+fs.ensureDirSync(buildFolderPath)
+
 // save compiled code to output
 let output = JSON.parse(solc.compile(JSON.stringify(input)));
-// export abi and bytecode compiled contract
-const interface = output.contracts["Campaign.sol"].Campaign.abi;
-const bytecode = output.contracts["Campaign.sol"].Campaign.evm.bytecode.object;
+// all contracts
+let contracts = output.contracts["Campaign.sol"]
 
-module.exports = {
-  interface,
-  bytecode,
-};
+// Go through all the contracts in the contracts object and write them to a json file
+for (let contract in contracts) {
+  // take the contract and save as a json file in the build folder
+   fs.outputJsonSync(
+     // parameters: path, filename +extension
+     path.resolve(buildFolderPath, contract + ".json"), contracts[contract])
 
-console.log(`Contract compiled successfully`);
+/*  this works too
 
-//console.log(output.contracts['Campaign.sol'].Campaign.evm.bytecode.object)
+  fs.writeFileSync(
+  // parameters: path, filename +extension
+  path.resolve(buildFolderPath, contract + ".json"), JSON.stringify(contracts[contract]))
+*/
+
+ 
+}
