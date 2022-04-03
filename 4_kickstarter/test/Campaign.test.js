8e461567aa0853a25a30abe7845c0dea8634f712
thelonehegelian <thelonehegelian@gmail.com>
2022-04-03 18:28:36 +0100

    deploy campaign contract through factory contract

--- 4_kickstarter/test/Campaign.test.js
+++ 4_kickstarter/test/Campaign.test.js
@@ -11,6 +11,45 @@ const web3 = new Web3(ganache.provider());
 const compiledFactoryContract = require("../ethereum/build/CampaignFactory.json");
 const compiledCampaignContract = require("../ethereum/build/CampaignFactory.json");
 
-{}
 
-console.log(Object.keys(contract))
+// variables
+let accounts;
+let factory;
+let campaign;
+let campaignAddress;
+
+//console.log(Object.keys(compiledFactoryContract.evm.bytecode))
+
+beforeEach(async () => {
+    // get acounts
+    accounts = await web3.eth.getAccounts();
+    
+    // deploy the contract factory contract
+    factory = await new web3.eth.Contract(compiledFactoryContract.abi) //contract abi
+      .deploy({
+        data: compiledFactoryContract.evm.bytecode.object, // contract bytecode
+      })
+      .send({ from: accounts[0], gas: "5000000" }); // have to increase gas limit to avoid 'out of gas error'. The contract is probably too large for gasLimit = '1000000'
+
+      // create campaign using the factory contract  
+      // createCampaign fucntion expects a value
+      await factory.methods.createCampaign('100').send({
+          from: accounts[0], gas: "1000000",
+      })
+
+      // get the address of the deployed campaign contract above
+     let addresses = await factory.methods.getDeployedContracts().call()
+     campaignAddress = addresses[0]
+
+     // create the newly created campaign contract from the contract address
+     campaign = await new web3.eth.Contract(compiledCampaignContract.abi, campaignAddress)
+
+  });
+
+// test to validate contract deployment
+describe("Factory", () => {
+    it("deploys a contract", () => {
+      // test to see if the contract has an address, if it does then it was deployed successfully
+      assert.ok(factory.options.address);
+    });
+})
