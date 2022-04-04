931f96a5d147a9bae2ccbbb09fd9afcb992dc1c5
thelonehegelian <thelonehegelian@gmail.com>
2022-04-04 22:29:19 +0100

    Fix name of the Campaign contract json file

--- 4_kickstarter/test/Campaign.test.js
+++ 4_kickstarter/test/Campaign.test.js
@@ -9,7 +9,7 @@ const web3 = new Web3(ganache.provider());
 // the compiled code is in JS Object format
 // JSON.parse won't be needed as parsed it during the compilation process in compile.js
 const compiledFactoryContract = require("../ethereum/build/CampaignFactory.json");
-const compiledCampaignContract = require("../ethereum/build/CampaignFactory.json");
+const compiledCampaignContract = require("../ethereum/build/Campaign.json");
 
 
 // variables
@@ -32,24 +32,35 @@ beforeEach(async () => {
       .send({ from: accounts[0], gas: "5000000" }); // have to increase gas limit to avoid 'out of gas error'. The contract is probably too large for gasLimit = '1000000'
 
       // create campaign using the factory contract  
-      // createCampaign fucntion expects a value
+      // createCampaign function expects a value
       await factory.methods.createCampaign('100').send({
-          from: accounts[0], gas: "1000000",
+          from: accounts[0], gas: "1000000", // accounts[0] should also be the contract manager
       })
 
       // get the address of the deployed campaign contract above
      let addresses = await factory.methods.getDeployedContracts().call()
      campaignAddress = addresses[0]
-
+      console.log(campaignAddress)
      // create the newly created campaign contract from the contract address
      campaign = await new web3.eth.Contract(compiledCampaignContract.abi, campaignAddress)
+      console.log(campaign.methods)
+      console.log(factory.methods)
 
   });
 
 // test to validate contract deployment
-describe("Factory", () => {
-    it("deploys a contract", () => {
+describe("Contracts", () => {
+    it("deploys contracts", () => {
       // test to see if the contract has an address, if it does then it was deployed successfully
-      assert.ok(factory.options.address);
+      assert.ok(campaign.options.address);
+      assert.ok(factory.options.address)
     });
+
+    it("sets the creator of the campaign as manager"), async () => {      
+      // get the address of the deployed contract. This is already done in on line 42 
+      // call the manager function on the contract. The campaign contract is created on line 45 that can be used to call methods on the campaign contract
+      const managerAddress = await campaign.methods.manager().call()
+      // get the address of the person who deployed the campaign contract. See line 36, the contract was deployed using accounts[0]
+      assert.equal(accounts[0], managerAddress)
+    }
 })
