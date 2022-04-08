a4075a2c24a4759cbcee36bd0abcd645a79e5042
thelonehegelian <thelonehegelian@gmail.com>
2022-04-08 03:21:02 +0100

    Deploy contract successfully

--- 4_kickstarter/ethereum/deploy.js
+++ 4_kickstarter/ethereum/deploy.js
@@ -1,9 +1,11 @@
+const fs = require("fs");
 // get environment variables from a .env file
 require("dotenv").config();
 // import web3 library
 const Web3 = require("web3");
-// get ABI and bytecode of the compiled contract
-const { interface, bytecode } = require("./compile");
+// get ABI and bytecode from the contract json files
+const campaignFactory = require("./build/CampaignFactory.json");
+
 // import wallet provider with which to access the accounts on MetaMask
 const HDWalletProvider = require("@truffle/hdwallet-provider");
 
@@ -28,28 +30,29 @@ const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
   console.log(`Attempting to deploy contract from ${accounts[0]}`);
   // Is there a transaction receipt at deployment?
-  const result = await new web3.eth.Contract(interface)
+  const result = await new web3.eth.Contract(campaignFactory.abi)
     .deploy({
-      data: bytecode,
+      data: campaignFactory.evm.bytecode.object,
     })
-    .send({ gas: "1000000", from: accounts[0] });
+    .send({ gas: "2000000", from: accounts[0] }); // gas should be enough to store a large contract like this otherwise error: The contract code couldn't be stored, please check your gas limit
 
+  // maybe export this to a text file
+  console.log(
+    `Contract deployed to the following address: ${result.options.address}`
+  );
+
+  // fs.writeFileSync("contractAddress.txt", str(result.options.address));
   /*
-  Maybe better to produce a json file with this information than console logging
-  */
-
-  let contract = JSON.stringify({
+    Don't need this anymore
+    
+    let contract = JSON.stringify({
     abi: interface,
     address: result.options.address,
   });
+
   fs.writeFileSync("contract.json", contract);
 
-  // console log contracts ABI
-  // console.log(interface);
-  // console logs the ethereum address of the deployed contract
-  // console.log(
-  //   `Contract deployed to the following address: ${result.options.address}`
-  // );
+  */
 
   // to prevent hanging developement
   provider.engine.stop();
