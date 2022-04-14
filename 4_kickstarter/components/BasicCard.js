3cf3fd7b6fd0285469fac1757a0f2937196674fc
thelonehegelian <thelonehegelian@gmail.com>
2022-04-14 21:19:46 +0100

    Render card component

---
+++ 4_kickstarter/components/BasicCard.js
@@ -0,0 +1,15 @@
+import { Card } from "antd";
+
+export default function BasicCard(props) {
+  return (
+    <div>
+      <Card
+        title= {props.items[0].name} // campaign name
+        extra= {props.items[0].link} // link to campaign 
+        style={{ width: 500 }}
+      >
+        <p>{props.items[0].header}</p> {/* Campaign address */}       
+      </Card>
+    </div>
+  );
+}
--- 4_kickstarter/pages/index.js
+++ 4_kickstarter/pages/index.js
@@ -1,36 +1,38 @@
-// Custom DatePicker that uses Day.js instead of Moment.js
-import DatePicker from '../components/DatePicker'
-
-
-
-export default function Home() {
-  return <div>Hello Old Friend
-
-    <div><DatePicker/></div>
-  </div>
-}
-/**
- 
 import React from "react";
 import factory from "../ethereum/factory";
+import BasicCard from "../components/BasicCard";
 
+let campaignName = "Little Bittle Campaign";
 class CampaignIndex extends React.Component {
-  // get data from the ethereum contract and set it as props
- // the function should resolve to an object   
+  // get data from the ethereum contract and set it as props for this component
+  // the function should resolve to an object
   static async getInitialProps() {
     // get the deployd campaign array from the factory contract
     const campaigns = await factory.methods.getDeployedContracts().call();
     console.log(campaigns);
     //  return the data
-     return (campaigns);
+    return { campaigns }; // should be returned using curly braces to give this.props[0] campaign name
   }
 
- 
+  renderCampaigns() {
+    console.log(this.props)
+    let items = this.props.campaigns.map((address) => {
+      return {
+        header: address,
+        name: campaignName,
+        link: <a>View Campaign</a>,
+
+      };
+    });
+    return <BasicCard items = {items}/>
+  }
 
   render() {
-    return <div>{this.props.campaings}</div>;
+    return (
+      <div>{this.renderCampaigns()}
+      </div>
+    );
   }
 }
 
 export default CampaignIndex;
-*/
