8b193caafe44697e4409a541b32f879f22acfa59
thelonehegelian <thelonehegelian@gmail.com>
2022-04-15 20:52:46 +0100

    Add basic form to the new campaign page

--- 4_kickstarter/pages/new.js
+++ 4_kickstarter/pages/new.js
@@ -1,12 +1,77 @@
-import React from 'react';
-import {Form, Layout} from 'antd'
+import React from "react";
+import { Form, Input, Button, Checkbox, Layout } from "antd";
 
 
-class CampaignNew extends React.Component {
-    render() {
-        return (
-            <Layout>
-            </Layout>
-        )
-    }
+const { Header, Footer, Sider, Content } = Layout;
+export default class CampaignNew extends React.Component {
+  render() {
+    return (
+      <Layout>
+          <Header>Navbar</Header>
+          <Content>
+        <Form
+          name="basic"
+          labelCol={{
+            span: 8,
+          }}
+          wrapperCol={{
+            span: 16,
+          }}
+          initialValues={{
+            remember: true,
+          }}
+          autoComplete="off"
+        >
+          <Form.Item
+            label="Username"
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: "Please input your username!",
+              },
+            ]}
+          >
+            <Input />
+          </Form.Item>
+
+          <Form.Item
+            label="Password"
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: "Please input your password!",
+              },
+            ]}
+          >
+            <Input.Password />
+          </Form.Item>
+
+          <Form.Item
+            name="remember"
+            valuePropName="checked"
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Checkbox>Remember me</Checkbox>
+          </Form.Item>
+
+          <Form.Item
+            wrapperCol={{
+              offset: 8,
+              span: 16,
+            }}
+          >
+            <Button type="primary" htmlType="submit">
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+        </Content>
+      </Layout>
+    );
+  }
 }
