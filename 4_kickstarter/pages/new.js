16ff7afbac401d2be135577bc05c8607324ce971
thelonehegelian <thelonehegelian@gmail.com>
2022-04-15 20:43:08 +0100

    Add layout to the new page

---
+++ 4_kickstarter/pages/new.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Form, Layout} from 'antd'
+
+
+class CampaignNew extends React.Component {
+    render() {
+        return (
+            <Layout>
+            </Layout>
+        )
+    }
+}
