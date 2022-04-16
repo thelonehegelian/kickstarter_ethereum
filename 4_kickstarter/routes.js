8755fdd90339c6cca84a62e129f97becfe402664
thelonehegelian <thelonehegelian@gmail.com>
2022-04-16 23:49:15 +0100

    Add a route for new campaign

--- 4_kickstarter/routes.js
+++ 4_kickstarter/routes.js
@@ -1,5 +1,7 @@
 const routes = require("next-routes")();
 
-routes.add("/campaigns/:address", "/show"); // renders show.js
+routes
+  .add("/campaigns/new", "/new") // render new page if this link is visited. I have not setup my routes like '/campaigns/new
+  .add("/campaigns/:address", "/show"); // renders show page
 
 module.exports = routes;
