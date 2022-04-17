6facc975a30d076de6251c1002ff3593957414a5
thelonehegelian <thelonehegelian@gmail.com>
2022-04-17 03:37:25 +0100

    Fix navbar typo

--- 4_kickstarter/ethereum/factory.js
+++ 4_kickstarter/ethereum/factory.js
@@ -1,8 +1,10 @@
-import web3 from './web3'
-import CampaignFactory from './build/CampaignFactory.json'
+import web3 from "./web3";
+import CampaignFactory from "./build/CampaignFactory.json";
 
 // deploy contract instance using the abi and contract address
-const campaignFactoryInstance = new web3.eth.Contract(CampaignFactory.abi, "0x06410DAFB24e77d3Dd82947d59c0b70f2018c1af")
+const campaignFactoryInstance = new web3.eth.Contract(
+  CampaignFactory.abi,
+  "0xFf36A8b4cC424e267d3Ed1807d5e82294f3ad394"
+);
 
-
-export default campaignFactoryInstance
+export default campaignFactoryInstance;
