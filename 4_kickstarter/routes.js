046897a9903ffc704a389437bee0a928eafbcc9e
thelonehegelian <thelonehegelian@gmail.com>
2022-04-20 18:34:47 +0100

    Add route to resquests index

--- 4_kickstarter/routes.js
+++ 4_kickstarter/routes.js
@@ -2,6 +2,7 @@ const routes = require("next-routes")();
 
 routes
   .add("/campaigns/new", "/new") // render new page if this link is visited. I have not setup my routes like '/campaigns/new
-  .add("/campaigns/:address", "/show"); // renders show page
+  .add("/campaigns/:address", "/show") // renders show page
+  .add("/campaigns/:address/requests", "requests/index"); //renders index.js in requests folder when the user visits campaign requests
 
 module.exports = routes;
