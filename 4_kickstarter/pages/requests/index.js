d2e43cdc9313fa1c9522720763ae692ad091881e
thelonehegelian <thelonehegelian@gmail.com>
2022-04-20 22:50:16 +0100

    Add Create Request button

--- 4_kickstarter/pages/requests/index.js
+++ 4_kickstarter/pages/requests/index.js
@@ -2,12 +2,22 @@
 
 import React from "react";
 import RequestsTable from "../../components/RequestsTable";
+import { Button } from "antd";
+import { Link } from "../../routes";
 
+// TODO: Add styles
 export default class RequestsIndex extends React.Component {
   render() {
     return (
       <>
-        <h1>Show Requests</h1>
+        <div>
+          <h1>Show Requests</h1>
+
+          {/* TODO: only show the button if the connected metmask address is of manager */}
+          <Link route="">
+            <Button type="primary">Add a Request</Button>
+          </Link>
+        </div>
         <RequestsTable />
       </>
     );
